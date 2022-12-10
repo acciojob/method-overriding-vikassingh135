2b224a898cfca41c6ca54e3d5b12632279a99da6
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2022-12-10 12:27:32 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,19 +1,20 @@
 package com.driver;
 
-class A {
+
+public class Main {
+  public static class A {
   String meth() {
     return "Invoking method from class A";
   }
 }
 
-class B extends A {
+public static class B extends A {
   String meth() {
     return "Method is overridden in Extendend class B";
   }
 }
 
-public class Main {
-  public static void main(String []args) throws Exception{ 
+ public static void main(String []args) throws Exception{ 
    B b = new B();
    b.meth();
   }
